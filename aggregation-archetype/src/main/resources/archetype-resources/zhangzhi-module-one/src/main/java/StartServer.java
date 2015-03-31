6954956af4900c74b420e0f0e6dfc80285068579
liuguly <lx_java@foxmail.com>
2015-03-31 23:31:50 +0800

    modify package

--- aggregation-archetype/src/main/resources/archetype-resources/zhangzhi-module-one/src/main/java/StartServer.java
+++ aggregation-archetype/src/main/resources/archetype-resources/zhangzhi-module-one/src/main/java/StartServer.java
@@ -1,9 +1,13 @@
 /*******************************************************************************
  * Copyright (c) 2005, 2014 springside.github.io
+
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  *******************************************************************************/
-package com.lx.myproject.example;
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
 
 import java.io.IOException;
 import java.util.Scanner;
--- aggregation-archetype/src/main/resources/archetype-resources/zhangzhi-module-one/src/test/java/repository/JpaMappingTest.java
+++ aggregation-archetype/src/main/resources/archetype-resources/zhangzhi-module-one/src/test/java/repository/JpaMappingTest.java
@@ -1,9 +1,13 @@
 /*******************************************************************************
  * Copyright (c) 2005, 2014 springside.github.io
+
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  *******************************************************************************/
-package com.lx.myproject.example.repository;
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.repository;
 
 import static org.assertj.core.api.Assertions.*;
 
